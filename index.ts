2d51f65c07299dabfb1d04d6b13c47e7b141bb34
Jashan Bansal <jashanbansal1010@gmail.com>
2026-06-22 14:46:56 +0530

    v1

---
+++ index.ts
@@ -0,0 +1,53 @@
+import express from "express";
+
+const app = express();
+
+const BALANCES = {
+
+}
+
+const ORDERBOOKS = {
+    SOL: {},
+    BTC: {}
+}
+
+app.post("/signup", (req,res) => {
+
+})
+
+app.post("signin" , (req,res) => {
+
+})
+
+app.post("/order" , (req,res) => {
+
+})
+
+
+
+
+
+
+app.get("/depth" , (req,res) =>{
+
+})
+
+app.get("/fills",(req,res) =>{
+
+})
+
+app.get("/orders",(req,res) =>{
+
+})
+
+app.get("order/orderId",(req,res) =>{
+
+})
+
+
+
+
+app.delete("/order/orderId",(req,res) =>{
+
+})
+
